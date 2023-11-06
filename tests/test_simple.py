1b0b331b5b65d072e4c757a6b92242d461c1bc75
Eike Middell <eike@middell.net>
2023-11-06 09:21:22 +0100

    use pytest for unit tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,17 +1,6 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
-import unittest
+import pytest
 
 from sample.simple import add_one
 
-
-class TestSimple(unittest.TestCase):
-
-    def test_add_one(self):
-        self.assertEqual(add_one(5), 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_one():
+    assert add_one(5) ==  6
